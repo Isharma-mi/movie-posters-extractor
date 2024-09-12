0e6b9560ce25e2995a752f51be08811ea4ddb3b9
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-09-12 16:04:52 -0400

    Results section and CSS styling
    - Updated documentation
    - Added results section showing which movies had posters downloaded and which didn't
    - Added proper csv reading
    - Added styling
    - NOTE: Some movie titles cause issues with the API where another movie poster is chosen

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Gets ref to submit button
 let submitBtn = document.getElementById("submitBtn");
-
 // Gets ref to csv file selected by user
 let fileName = document.getElementById("fileName");
+// Gets ref to results section
+let results = document.getElementById("results");
 
-// Reads the CSV file
+/**
+ * Reads the .csv file
+ */
 function readCSV() {
-    console.log("TESTING: Reading file");
     // Gets the file
     const file = fileName.files[0];
 
     // Ensures file was selected
     if (file) {
-        console.log("TESTING: Received a CSV.");
-        
         // Handles file reading asynchronously
         const reader = new FileReader();
 
@@ -29,30 +29,41 @@ function readCSV() {
         
         // Reads the file as a text string -> Calls onload
         reader.readAsText(file);
+    } else {
+        console.log("No file selected!");
     }
 }
 
+/**
+ * Modifies data for easier reading 
+ * @param {String} csvText - Text from the csv file
+ */
 function parseCSV(csvText) {
-    console.log("TESTING: Parsing CSV");
     /* Array consiting of each separeted row with line endings for windows/mac removed
         - Delimiters (/.../): Define regex pattern
         - \r\n: carriage return and new line
         - \g: global search -> Looks across entirety of string
     */
-    const movies = csvText.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split("\n");
+    const movies = csvText.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split(/[,\n]/);
     // Tracker is used as prefix for file name
     let tracker = 1;
 
     // Loops thru each movie
     for (const movie of movies) {
         // Gets the poster
-        getMoviePoster(movie, tracker);
+         getMoviePoster(movie, tracker);
         // Increments tracker
         tracker++;
     }
 }
 
-function getMoviePoster(movie, filePrefix) {
+/**
+ * Gets the poster for a specific movie. 
+ * Async to update results in order of movies.
+ * @param {String} movie - Name of the movie
+ * @param {String} filePrefix - Prefix for poster file name
+ */
+async function getMoviePoster(movie, filePrefix) {
     let url = `http://www.omdbapi.com/?t=${movie}&apikey=${key}`;
     
     // Gets the data related to the movie
@@ -61,7 +72,7 @@ function getMoviePoster(movie, filePrefix) {
         .then(data => {
             // Gets the data in binary format and to deal with cross-origin requests
                 // Poster url domain is different from OMDb API
-            fetch(data.Poster)
+             fetch(data.Poster)
                 .then(resp => resp.blob())
                 .then(blob => {
                     const url = window.URL.createObjectURL(blob);
@@ -77,10 +88,14 @@ function getMoviePoster(movie, filePrefix) {
                     posterLink.click();
                     // Removes posterLink element from HTMl
                     document.body.removeChild(posterLink);
+                    // Updates html to show poster downloaded
+                    results.innerHTML += `Downloaded poster for ${data.Title}<br>`;
                 }).catch(() => {
-                    console.log(`ERROR: Unable to fetch poster for movie: ${data.Title}`);
+                    // Filters out any errors server side
+                    if (!movie === "undefined") {
+                        results.innerHTML += `ERROR: Unable to download poster for ${movie}<br>`;    
+                    }
                 });
-            console.log(data.Poster);
         }).catch (() => {
             console.log(`ERROR: Unable to fetch ${movie}`);
         });
