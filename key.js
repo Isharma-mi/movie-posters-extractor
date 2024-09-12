c177f3712f2b3cf732b2811a785c35ab086bff66
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-09-12 12:45:25 -0400

    Able to extract posters
    - Able to extract posters via an array in the index.html code
    - TODO: Pass in a csv file and get the posters from there
    - TODO: Add posters in a folder to prevent clutter in downloads

---
+++ key.js
@@ -0,0 +1,2 @@
+// NOTE: Do NOT commit this to github
+const key = "insert_key_here";
---
+++ script.js
@@ -0,0 +1,65 @@
+// TODO: Get movies from csv file
+let movies = ["Dark Knight", "Primer", "Arrival"];
+
+// Gets ref to submit button
+let submitBtn = document.getElementById("submitBtn");
+
+/**
+ * Gets the movie poster
+ */
+let getMoviePoster = () => {
+    console.log("TESTING: Getting movies");
+    let tracker = 0;
+
+    // Loop thru each movie
+    for (const movie of movies) {
+        let url = `http://www.omdbapi.com/?t=${movie}&apikey=${key}`;
+        // Gets the data related to the movie
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+
+                // Gets the data in binary format and to deal with cross-origin requests
+                    // Poster url domain is different from OMDb API
+                fetch(data.Poster)
+                    .then(resp => resp.blob())
+                    .then(blob => {
+                        const url = window.URL.createObjectURL(blob);
+                        // Creates element used to download image
+                        const posterLink = document.createElement('a');
+                        // Sets element's link equal to the url
+                        posterLink.href = url;
+                        // Sets the name of the file downloaded
+                        posterLink.download = `${movie}_Poster`;
+                        // Adds posterLink element to html
+                        document.body.appendChild(posterLink);
+                        // Clicks element to download image
+                        posterLink.click();
+                        // Removes posterLink element from HTMl
+                        document.body.removeChild(posterLink);
+                    }).catch(() => {
+                        console.log(`ERROR: Unable to fetch poster for movie: ${data.Title}`);
+                    });
+
+                console.log(data.Poster);
+                
+              
+
+            }).catch (() => {
+                console.log(`ERROR: Unable to fetch ${movie}`);
+            });
+        tracker++;
+    }
+}
+
+submitBtn.addEventListener("click", getMoviePoster);
+
+
+
+# Movie Poster Extractor
+HTML/CSS/JS Code used to get movie posters from OMDb API from a CSV file (WIP).
+
+## NOTE: 
+- Information from OMDb API(https://www.omdbapi.com/) used. Need to generate and insert key into code to work.
+1. Will need to generate key through them
+2. Insert the key in the "insert_key_here" from the key.js file.
