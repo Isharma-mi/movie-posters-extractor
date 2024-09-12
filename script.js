ac8b2c2bc7bec62595645c9c55771763a51775fd
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-09-12 12:47:26 -0400

    Updated readme

--- script.js
+++ script.js
@@ -53,13 +53,3 @@ let getMoviePoster = () => {
 }
 
 submitBtn.addEventListener("click", getMoviePoster);
-
-
-
-# Movie Poster Extractor
-HTML/CSS/JS Code used to get movie posters from OMDb API from a CSV file (WIP).
-
-## NOTE: 
-- Information from OMDb API(https://www.omdbapi.com/) used. Need to generate and insert key into code to work.
-1. Will need to generate key through them
-2. Insert the key in the "insert_key_here" from the key.js file.
